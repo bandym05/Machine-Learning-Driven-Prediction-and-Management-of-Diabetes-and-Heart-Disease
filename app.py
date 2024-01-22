c67794cbae43a89046560b075c61d93c7eab7b25
Bandile Malaza <bandymalaza05@gmail.com>
2024-01-22 10:47:56 +0000

    set5h

--- app.py
+++ app.py
@@ -2,7 +2,19 @@ from flask import Flask, render_template, request
 import numpy as np
 from joblib import load
 
+from flask import Flask,render_template,request,redirect,session,flash,url_for
+from functools import wraps
+from flask_mysqldb import MySQL
+
 app = Flask(__name__)
+
+#database credentials
+# app.config['MYSQL_HOST']='localhost'
+# app.config['MYSQL_USER']='root'
+# app.config['MYSQL_PASSWORD']=''
+# app.config['MYSQL_DB']='db_sample'
+# app.config['MYSQL_CURSORCLASS']='DictCursor'
+# mysql=MySQL(app)
 
 # loading the saved models
 diabetes_model = load('diabetes/diabetes_model.joblib')
@@ -117,6 +129,63 @@ def heart_disease():
         return render_template('heart_disease.html', diagnosis=heart_diagnosis)
     else:
         return render_template('heart_disease.html')
+
+
+#Login
+@app.route('/') 
+@app.route('/login',methods=['POST','GET'])
+def login():
+    status=True
+    if request.method=='POST':
+        email=request.form["email"]
+        pwd=request.form["upass"]
+        cur=mysql.connection.cursor()
+        cur.execute("select * from users where EMAIL=%s and UPASS=%s",(email,pwd))
+        data=cur.fetchone()
+        if data:
+            session['logged_in']=True
+            session['username']=data["UNAME"]
+            flash('Login Successfully','success')
+            return redirect('home')
+        else:
+            flash('Invalid Login. Try Again','danger')
+    return render_template("login.html")
+
+#check if user logged in
+def is_logged_in(f):
+	@wraps(f)
+	def wrap(*args,**kwargs):
+		if 'logged_in' in session:
+			return f(*args,**kwargs)
+		else:
+			flash('Unauthorized, Please Login','danger')
+			return redirect(url_for('login'))
+	return wrap
+
+#Registration  
+@app.route('/reg',methods=['POST','GET'])
+def reg():
+    status=False
+    if request.method=='POST':
+        name=request.form["uname"]
+        email=request.form["email"]
+        pwd=request.form["upass"]
+        cur=mysql.connection.cursor()
+        cur.execute("insert into users(UNAME,UPASS,EMAIL) values(%s,%s,%s)",(name,pwd,email))
+        mysql.connection.commit()
+        cur.close()
+        flash('Registration Successfully. Login Here...','success')
+        return redirect('login')
+    return render_template("reg.html",status=status)
+
+#logout
+@app.route("/logout")
+def logout():
+	session.clear()
+	flash('You are now logged out','success')
+	return redirect(url_for('login'))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
